3a223eb0526e1f71d2e37135ca73d2647ae24131
sjKim <ksj0818@github.com>
2023-07-04 19:54:54 +0900

    마지막 p태그 삭제 및 버튼css 변경

--- src/components/posts/PostsCard.tsx
+++ src/components/posts/PostsCard.tsx
@@ -6,18 +6,16 @@ import { AiTwotoneCalendar } from "react-icons/ai";
 export default function PostsCard({ post }: { post: Post }) {
   const { path, title, description, category, date } = post;
   return (
-    <article className="text-center transition-all shadow-lg hover:-translate-y-1 hover:shadow-xl">
-      <Link href={`/posts/${path}`} className="">
+    <article className="text-center transition-all shadow-lg hover:-translate-y-2 hover:shadow-xl hover:brightness-90">
+      <Link href={`/posts/${path}`}>
         <Image src={`/images/posts/${path}.png`} alt={path} width={300} height={200} className="w-full" />
         <p className="flex items-center justify-end font-bold gap-x-1 text-sky-500">
           <AiTwotoneCalendar />
           {date.toString()}
         </p>
         <h2 className="text-xl font-bold truncate">{title}</h2>
-        <h3 className="font-semibold truncate ">{description}</h3>
-        <p className="my-2">
-          <button className="px-2 py-1 rounded-lg bg-emerald-300">{category}</button>
-        </p>
+        <h3 className="text-lg font-semibold truncate">{description}</h3>
+        <button className="px-2 py-1 my-2 rounded-lg bg-emerald-300">{category}</button>
       </Link>
     </article>
   );
