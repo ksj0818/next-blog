2fd5c434ca89f222fd49b9b7fd22421ee2c37227
sjKim <ksj0818@github.com>
2023-07-06 20:09:34 +0900

    현재,이전,다음  포스트 가져오는 로직

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,6 +10,8 @@ export type Post = {
   featured: boolean;
 };
 
+export type PostData = Post & { content: string; prev: Post | null; next: Post | null };
+
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), "data", "posts.json");
   return readFile(filePath, "utf-8")
@@ -24,4 +26,17 @@ export async function getFeaturedPosts() {
 export async function getNonFeaturedPosts() {
   const data = await getAllPosts();
   return data.filter((post) => !post.featured);
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const content = await readFile(filePath, "utf-8");
+  const currentIdx = posts.indexOf(post);
+  const prev = currentIdx < posts.length ? posts[currentIdx + 1] : null;
+  const next = currentIdx > 0 ? posts[currentIdx - 1] : null;
+
+  return { ...post, content, prev, next };
 }
