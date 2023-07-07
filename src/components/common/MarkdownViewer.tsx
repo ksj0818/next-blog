a9b80c82d7218750b953877bf73872938e3657f1
sjKim <ksj0818@github.com>
2023-07-07 15:24:46 +0900

    빌드 시 Do not pass children as props error 수정

--- src/components/common/MarkdownViewer.tsx
+++ src/components/common/MarkdownViewer.tsx
@@ -6,37 +6,32 @@ import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <>
-      <ReactMarkdown
-        children={content}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-            return !inline && match ? (
-              <SyntaxHighlighter
-                {...props}
-                children={String(children).replace(/\n$/, "")}
-                style={dracula}
-                language={match[1]}
-                PreTag="div"
-              />
-            ) : (
-              <code {...props} className={className}>
-                {children}
-              </code>
-            );
-          },
-          img: (image) => (
-            <Image
-              src={image.src || ""}
-              alt={image.alt || ""}
-              width={760}
-              height={500}
-              className="w-full max-h-[400px]"
-            />
-          ),
-        }}
-      />
-    </>
+    <ReactMarkdown
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter {...props} style={dracula} language={match[1]} PreTag="div">
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={760}
+            height={500}
+            className="w-full max-h-[400px]"
+          />
+        ),
+      }}
+    >
+      {content}
+    </ReactMarkdown>
   );
 }
